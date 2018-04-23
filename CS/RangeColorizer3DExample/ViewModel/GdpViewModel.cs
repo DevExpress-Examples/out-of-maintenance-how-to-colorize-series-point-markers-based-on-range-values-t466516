bd9170e71ee9d54a832a3a44cdaa719c102d60ce
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/RangeColorizer3DExample/ViewModel/GdpViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Resources;
+using System.Xml.Linq;
+
+namespace RangeColorizer3DExample.ViewModel {
+    public class Gdp {
+        public string Country { get; private set; }
+        public int Year { get; private set; }
+        public double Value { get; private set; }
+        public string Region { get; private set; }
+
+        public Gdp(string country, int year, double value, string region) {
+            Country = country;
+            Year = year;
+            Value = value;
+            Region = region;
+        }
+    }
+
+    public class GdpViewModel {
+        public IEnumerable<Gdp> Gdps { get; private set; }
+
+        public GdpViewModel() {
+            Gdps = new GdpResourceLoader("Data/GdpStatistics.xml").Load();
+        }
+    }
+
+    public class GdpResourceLoader {
+        string Filepath { get; set; }
+
+        public GdpResourceLoader(string filepath) {
+            Filepath = filepath;
+        }
+
+        public IEnumerable<Gdp> Load() {
+            Collection<Gdp> result = new Collection<Gdp>();
+
+            Uri resourceUri = new Uri(Filepath, UriKind.RelativeOrAbsolute);
+            StreamResourceInfo resourceInfo = Application.GetResourceStream(resourceUri);
+            XDocument xDoc = XDocument.Load(resourceInfo.Stream);
+            foreach (var xGdp in xDoc.Element("Statistics").Elements("Gdp")) {
+                result.Add(new Gdp(
+                    country: xGdp.Attribute("Country").Value,
+                    year: Convert.ToInt32(xGdp.Attribute("Year").Value),
+                    value: Convert.ToDouble(xGdp.Attribute("Value").Value),
+                    region: xGdp.Attribute("Region").Value
+                ));
+            }
+            return result;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to colorize series point markers based on range values
+
+
+This example demonstrates how to colorize series point markers based on range values.
+
+
+<h3>Description</h3>
+
+<p>To do this, assign a&nbsp;<a href="https://documentation.devexpress.com/#WPF/clsDevExpressXpfChartsRangeColorizer3Dtopic">RangeColorizer3D</a>&nbsp;object to the&nbsp;<a href="https://documentation.devexpress.com/#WPF/DevExpressXpfChartsSeries3DViewBase_Colorizertopic">Series3DViewBase.Colorizer</a>&nbsp;property. Then, configure range stops using the&nbsp;<a href="https://documentation.devexpress.com/#WPF/DevExpressXpfChartsRangeColorizer3D_RangeStopstopic">RangeColorizer3D.RangeStops</a>&nbsp;property and optionally, configure required colors using the&nbsp;<a href="https://documentation.devexpress.com/#WPF/DevExpressXpfChartsPaletteColorizer3DBase_Palettetopic">PaletteColorizer3DBase.Palette</a>&nbsp;property. Finally, specify which values should be measured - you can do this using the&nbsp;<a href="https://documentation.devexpress.com/#WPF/DevExpressXpfChartsRangeColorizer3D_ValueProvidertopic">RangeColorizer3D.ValueProvider</a>&nbsp;property.</p>
+
+<br/>
+
+
